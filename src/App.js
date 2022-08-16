c7574ea8b9273b27d2f3c1c0ff478a67d7a6df27
Hu Kaixiang <maxhu787@gmail.com>
2022-08-17 00:03:32 +0800

    clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+function getRealTime() {
+  const currentTime = Date.now();
+  console.log(new Date(Math.round(currentTime / 1000) * 1000), currentTime);
+  return (Math.floor(currentTime / 1000) + 1) * 1000 - currentTime;
+}
+
+(async function () {
+  let reduceTime = 0;
+  while (true) {
+    reduceTime = getRealTime();
+    await sleep(reduceTime);
+  }
+})()
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};*/
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateDate = this.updateDate.bind(this)
+    
+    this.state = {
+      time: new Date().toLocaleTimeString()
+    }
+    this.interval = setInterval(this.updateDate, 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  updateDate() {
+    this.setState({
+      time: new Date().toLocaleTimeString(),
+    })
+  }
+  render() {
+    return (
+      <div id="app">
+        <p>Test React clock</p>
+        <div id="time">{this.state.time}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
